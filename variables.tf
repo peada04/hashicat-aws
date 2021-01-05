3f2f0841864feb049d51600b142ece3dd98fda47
peada04 <dpeacock@hashicorp.com>
2021-01-04 22:27:02 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -34,12 +34,6 @@ variable "ttl" {
   description = "time to live before reaper should delete"
 }
 
-
-variable "region" {
-  description = "The region where the resources are created."
-  default     = "us-east-1"
-}
-
 variable "address_space" {
   description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
   default     = "10.0.0.0/16"
